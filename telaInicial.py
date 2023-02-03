4cd639babf6974cd06e9750321cba0f2da914a33
Pedrowwww <pedrowwww44@gmail.com>
2023-02-03 13:59:07 -0300

    first commit

---
+++ telaInicial.py
@@ -0,0 +1,75 @@
+import telas
+def telaInicial(values, evento):
+    if runCheck(values):
+        if evento == "regBD":
+            nome = telas.createWindow(2, "save")
+            saveEntry(values, nome)
+    else:
+        #constroi a menssagem de erro
+        plural = True
+        done = False
+        c = 1
+        #define o offset para localizar a vírgula
+        empty = 0
+        for key in values:
+            if values[key]=="":
+                empty+=1
+
+        if not values["Banco"] and empty == 2:
+            plural = False
+        if values["Banco"]:
+            if empty == 1:
+                plural = False
+            empty+=1
+
+        offset = len(values)-empty
+
+        while(True and not done):
+            if(plural):
+                erro = "Os campos: "
+
+                for key in values:
+                    if not values[key] and key!="Banco":
+                        c+=1
+                        erro += key.lower()
+                            
+                        if len(values) - c > 1+offset:
+                            erro+=","
+                        elif len(values) - c > offset:
+                            erro+=" e"
+                        
+                        erro+=" "
+
+                    if (key == "Banco"):
+                        erro += "têm que ser preenchidos"
+                        done = True
+                        break
+            else:
+                erro = "O campo "
+                for key in values:
+                    if not values[key] and key != "Banco":
+                        erro += key.lower()
+                erro += " tem que ser preenchido"
+                break
+        return erro
+
+def runCheck(values):
+    #confere se os campos foram preenchidos
+    for key in values:
+        if not values[key]:
+            #se o valor vazio não for do campo -BD-, retorne falso
+            if not key == "Banco":
+                return False      
+    return True
+
+def saveEntry(values, nome):
+    filename = "Entries/"+nome["nome"]+".txt"
+    file = open(filename, "a")
+    file.write(
+f"""Usuario: {values["Usuário"]}
+Senha: {values["Senha"]}
+Host: {values["Host"]}
+Banco: {values["Banco"]}"""
+    )
+
+    file.close()
---
+++ telas.py
@@ -0,0 +1,66 @@
+import PySimpleGUI as sg
+import telaInicial as telaInc
+
+sg.theme("DarkAmber")
+
+def openWindow(nome, layout):
+    c=0
+    janela = sg.Window(nome, layout, element_justification='c')
+
+    while True:
+        evento, valores = janela.read()
+        if evento == "telaInc" or evento == "regBD":
+            erro = telaInc.telaInicial(valores, evento)
+            if erro:
+                janela.Element("-TITLE-").update(erro, text_color = "Red")
+            if evento=="telaInc":
+                break
+        elif evento=="regNome":
+            janela.close()
+            return valores
+        else:
+            break
+    
+    janela.close()
+
+def createWindow(window, origem=""):
+    #layout da tela inicial
+    if (window == 1):
+        layout_coluna_input = [
+            [sg.InputText(key="Host")],
+            [sg.InputText(key="Usuário")],
+            [sg.InputText(key="Senha")],
+            [sg.InputText(key="Banco")]
+        ]
+
+        layout_coluna_text = [
+            [sg.Text("Host: ")],
+            [sg.Text("Usuário: ")], 
+            [sg.Text("Senha: ")],
+            [sg.Text("Banco de Dados: ")]
+        ]
+
+        layout_frame_registros = [
+            [sg.T("Acessos Salvos")]
+        ]
+
+        layout = [
+            [sg.Text("Bem-Vindo!", key="-TITLE-")],
+            [sg.Column(layout_coluna_text, vertical_alignment="left", justification="left"), sg.Column(layout_coluna_input, vertical_alignment="right", justification="right")],
+            [sg.Button("Salvar", key = "regBD"), sg.Button("Pronto", key = "telaInc")]
+        ]
+
+        nome = "Acessador de Banco de Dados"
+    if(window == 2):
+        if origem == "save":
+            layout = [
+                [sg.T("Defina um nome para salvar este endereço de acesso.")],
+                [sg.InputText(key="nome")],
+                [sg.Button("Pronto", key="regNome")]
+            ]
+
+            nome="Nome do Registro"
+
+    values = openWindow(nome, layout)
+    if values:
+        return values       
